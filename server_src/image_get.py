fd968207516f5d82ddc7d0cd4e173423d42e561a
sharabhuiyan <sbhuiyan@mit.edu>
2022-04-13 17:48:59 -0400

    response formatted as json

--- server_src/image_get.py
+++ server_src/image_get.py
@@ -1,4 +1,4 @@
-
+import json
 from PIL import Image as Image
 
 
@@ -40,8 +40,9 @@ def request_handler(request):
             return "Error: image is missing"
     image = load_color_image(f'/var/jail/home/team43/laser_comms/{image_}')
     data = [get_pixel(image, x, y) for x in range(image['width']) for y in range(image['height'])]
-    
-    return rgb565_convert(data)
+    converted_data = rgb565_convert(data)
+    response = {'pixels': converted_data}
+    return json.dumps(response)
     
 
         
